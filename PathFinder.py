4995027ca7c8123edb63eab26efdfb2b2587de62
Artem Sanakoev <enorone@gmail.com>
2015-11-24 06:34:50 +0100

    cometics

--- PathFinder.py
+++ PathFinder.py
@@ -55,7 +55,7 @@ def _find_path(graph, start_point, prev, end_point):
     :return: path from start_point to end_point. Each point is linear index
     """
     # print start_point, prev, end_point
-    n = len(graph)gst
+    n = len(graph)
     q = deque()
     q.appendleft(start_point)
     visited = n * [False]
